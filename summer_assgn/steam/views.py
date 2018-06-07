2ef403f247ad5b5519aff9247f749a9f172308c6
Aniket <aniket14goyal@gmail.com>
2018-06-07 23:03:02 +0530

    home 2 written

--- summer_assgn/steam/views.py
+++ summer_assgn/steam/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import views,login, authenticate
+from django.contrib.auth import views,login as loginu, authenticate
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -24,8 +24,14 @@ def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            return redirect(home)
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            loginu(request,user)
+            return home2(request)
     else:
         form = AuthenticationForm()
     return render(request, 'steam/login.html', {'form': form})
- 
+
+def home2(request):
+    return render(request,'steam/home2.html')
